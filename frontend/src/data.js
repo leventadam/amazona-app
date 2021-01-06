193be35318a48d6f3f931dc38a11857eed77f296
leventadam <leventaltunoz@gmail.com>
2021-01-06 21:40:04 +0300

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,78 @@
+const data={
+    products:[
+        {
+            _id: '1',
+            name:'Nike Tshirt',
+            category: 'Shirts',
+            image:'/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Kaliteli Ürün'
+
+        },
+        {
+            _id: '2',
+            name:'Lacoste Tshirt',
+            category: 'Shirts',
+            image:'/images/p2.jpg',
+            price: 130,
+            brand: 'Lacoste',
+            rating: 4,
+            numReviews: 8,
+            description: 'Kaliteli Ürün'
+
+        },
+        {
+            _id: '3',
+            name:'Nike pants slim',
+            category: 'Pants',
+            image:'/images/p3.jpg',
+            price: 100,
+            brand: 'Nike',
+            rating: 5,
+            numReviews: 10,
+            description: 'Kaliteli Ürün'
+
+        },
+        {
+            _id: '4',
+            name:'Adidas pants',
+            category: 'Pants',
+            image:'/images/p4.jpg',
+            price: 140,
+            brand: 'adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Kaliteli Ürün'
+
+        },
+        {
+            _id: '5',
+            name:'Adidas Tshirt',
+            category: 'Shirts',
+            image:'/images/p5.jpg',
+            price: 110,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'Kaliteli Ürün'
+
+        },
+        {
+            _id: '6',
+            name:'Converse Tshirt',
+            category: 'Shirts',
+            image:'/images/p6.jpg',
+            price: 120,
+            brand: 'Converse',
+            rating: 4.5,
+            numReviews: 13,
+            description: 'Kaliteli Ürün'
+
+        }
+       
+    ]
+}
+export default data;
